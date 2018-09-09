bd93805aefd6266ecc4f5e0a5bcab9f45ff4409b
Michal Orsak <xorsak01@fit.vutbr.cz>
2018-09-09 11:03:52 +0200

    hwio_select_devs_from_vector: use const &

--- src/hwio_cli.h
+++ src/hwio_cli.h
@@ -12,7 +12,7 @@ const char * hwio_help_str();
 
 ihwio_bus * hwio_init(int & argc, char * argv[]);
 
-std::vector<ihwio_dev*> hwio_select_devs_from_vector(std::vector<ihwio_dev*> devices, int index);
+std::vector<ihwio_dev*> hwio_select_devs_from_vector(const std::vector<ihwio_dev*>& devices, int index);
 
 char ** copy_argv(int argc, char * argv[], std::vector<void *> & to_free);
 
