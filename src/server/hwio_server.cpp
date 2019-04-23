8c7dd502b1d7d87281c0121839c3c2d70dd60f21
Vlastimil Košař <ikosar@fit.vutbr.cz>
2019-04-23 20:33:17 +0200

    Reset curr_ptr in all cases

--- src/server/hwio_server.cpp
+++ src/server/hwio_server.cpp
@@ -386,14 +386,15 @@ void HwioServer::handle_client_requests(int sd) {
 bool HwioServer::read_from_socket(ClientInfo * client) {
     char * rd_ptr = client->rx_buffer.buffer;
     size_t rd_len = RxBuffer::RX_BUFFER_SIZE;
-
+    
     if (client->rx_buffer.curr_len > 0) {
         if (client->rx_buffer.curr_ptr != client->rx_buffer.buffer) {
-           client->rx_buffer.curr_ptr = static_cast<char*>(memcpy(client->rx_buffer.buffer, client->rx_buffer.curr_ptr, client->rx_buffer.curr_len));
+           memcpy(client->rx_buffer.buffer, client->rx_buffer.curr_ptr, client->rx_buffer.curr_len);
         }
         rd_ptr = client->rx_buffer.curr_ptr + client->rx_buffer.curr_len;
         rd_len -= client->rx_buffer.curr_len;
     }
+    client->rx_buffer.curr_ptr = client->rx_buffer.buffer;
     std::cout << "read_from_socket:" << (void*)client->rx_buffer.buffer << " " << (void*)rd_ptr << " " << rd_len << std::endl;
     while (true) {
         errno = 0;
@@ -428,7 +429,7 @@ void HwioServer::parse_msgs(ClientInfo * client) {
         size_t msg_len = sizeof(Hwio_packet_header) + header->body_len;
         if (msg_len <= client->rx_buffer.curr_len) {
             rx_buffer = client->rx_buffer.curr_ptr + header->body_len;
-            std::cout << "parse_msgs:" << msg_len << " " <<  header->command << " " << header->body_len << " " << (void*)rx_buffer << std::endl;
+            std::cout << "parse_msgs:" << msg_len << " " <<  (int)header->command << " " << header->body_len << " " << (void*)rx_buffer << std::endl;
             respMeta = handle_msg(client, *header);
             if (respMeta.tx_size) {
                 size_t bytesWr = 0;
